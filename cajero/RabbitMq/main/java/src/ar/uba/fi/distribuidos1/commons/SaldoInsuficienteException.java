72dfac0885040322b77563621c69659f5253c0e1
Jonathan <jonathan.tierno@gmail.com>
2016-11-01 23:45:18 -0300

    limpieza

--- cajero/RabbitMq/main/java/src/ar/uba/fi/distribuidos1/commons/SaldoInsuficienteException.java
+++
@@ -1,9 +0,0 @@
-package ar.uba.fi.distribuidos1.commons;
-
-import java.rmi.RemoteException;
-
-public class SaldoInsuficienteException extends RemoteException {
-
-	private static final long serialVersionUID = -5517674320619883320L;
-
-}
