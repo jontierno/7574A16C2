f3ceedb53a235b36206ea1e0420c6f79ad37d0bb
Jonathan <jonathan.tierno@gmail.com>
2017-02-11 22:52:25 -0300

    refactor del productor

--- middleware/etapa3/traductor.cxx
+++ middleware/etapa3/traductor.cxx
@@ -4,6 +4,7 @@
 
 
 
+//Translates a word
 string translate(string word) {
 	string result = "";
 	for (int i =  0; i < word.size() ; i++){
@@ -18,18 +19,29 @@ string translate(string word) {
 }
 
 int main(int argc, char *argv[]) {
+	//Log init
     init_logger("Traductor", getpid());
     log_exit();
+
+    //Translator init
 	safelog("Iniciando Traductor...\n");
 	srand(getpid());
 	string word;
 	string result;
+
 	while (1) {
+		//Ask for a new task
 		word = getTask();
 		safelog("Trabajo recibido: %s\n", word.c_str());
+
+		//random translation time
 		sleep(rand()%10);
+
+		//translate
 		result = translate(word);
 		safelog("Trabajo traducido a: %s\n", result.c_str());
+
+		//returning result
         sendTask(result);
 	}
 
